04dbb38927f29d07fe95a519b16dcbb499bb2db7
vishvajeet shukla <djritikshukla@gmail.com>
2024-11-28 15:17:44 +0530

    functionn compo and class compo

--- src/compo and element/ClassCompo.jsx
+++ src/compo and element/ClassCompo.jsx
@@ -1,7 +1,23 @@
-class Greeting extends React.Component{
-    render() {
-        return (
-            <h1>{`Hello, ${this.props.message}`}</h1>;
-        );
-    }
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
 }
+
+export default Counter;
