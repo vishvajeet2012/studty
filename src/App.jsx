9886a7e3fa105d7b0a81d75923b58824dfdbbb66
vishvajeet shukla <djritikshukla@gmail.com>
2024-11-28 15:35:43 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PureFunction from './Function/PureFunction'
+import ClassCompo from './compo and element/ClassCompo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <PureFunction/>
+      {/* <PureFunction/> */}
+      <ClassCompo/>
+    
     </>
   )
 }
